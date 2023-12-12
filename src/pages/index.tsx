0e3b7da8ad99b36ba1d6238831784c3b8dc05252
jianrong7 <ipwnjianrong@gmail.com>
2023-12-12 10:05:01 +0800

    remove unnecessary import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Success from "./donation/success";
 import { CampaignCard } from "@/components/shared/CampaignCard";
 
 const inter = Inter({ subsets: ["latin"] });
