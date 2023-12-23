3edcd4b7b45f10cc7f6ee687d7f4eb17f8843c37
James <jamessinmaojun@gmail.com>
2023-12-23 17:59:39 +0800

    Fixed negative day bug and added carousel

--- src/components/shared/CampaignCard.tsx
+++ src/components/shared/CampaignCard.tsx
@@ -2,6 +2,10 @@
 
 import Image from "next/image";
 
+import { useRef } from "react";
+
+import { register } from "swiper/element/bundle";
+
 import {
   CardContent,
   CardDescription,
@@ -14,34 +18,56 @@ import { Button } from "@/components/ui/CampaignCardUI/button";
 import { Progress } from "@/components/ui/CampaignCardUI/progress";
 
 type CampaignCardProps = {
-  campaignTitle: string;
-  coverImageURL: string;
-  currentAmount: number;
-  targetAmount: number;
-  targetDate: number;
+  campaignTitle?: string;
+  coverImagesURLs?: string[];
+  currentAmount?: number;
+  targetAmount?: number;
+  targetDate?: number;
 };
 
+register()
+
 export const CampaignCard = ({
-  campaignTitle,
-  coverImageURL,
-  currentAmount,
-  targetAmount,
-  targetDate,
+  campaignTitle="Untitled",
+  coverImagesURLs=[],
+  currentAmount=0,
+  targetAmount=0,
+  targetDate=0,
 }: CampaignCardProps) => {
   const completionPercentage = Math.floor((currentAmount / targetAmount) * 100);
   const daysLeftToTarget = Math.floor((targetDate - Date.now()) / 8.64e7);
 
+  const swiperElRef = useRef(null)
+
   return (
     <div className="pt-6 bg">
       <div className="mx-6 rounded-lg overflow-hidden">
-        <Image
-          className="w-full h-auto max-h-60 object-cover"
-          src={coverImageURL}
-          alt="campaign cover image"
-          width={0}
-          height={0}
-          sizes="100vw"
-        />
+        <swiper-container
+          ref={swiperElRef}
+          navigation="true"
+          pagination="true"
+          style={{
+            "--swiper-pagination-bullet-inactive-color": "#CBD5E1",
+            "--swiper-pagination-color": "#F8FAFC",
+            "--swiper-navigation-color": "#F8FAFC",
+            "--swiper-navigation-size": "32px",
+          }}
+        >
+          {
+            coverImagesURLs.map(url =>
+                <swiper-slide key={url}>
+                  <Image
+                    className="w-full h-auto max-h-60 object-cover"
+                    src={url}
+                    alt="campaign cover image"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                  />
+                </swiper-slide>
+              )
+          }
+        </swiper-container>
       </div>
       <CardHeader>
         <CardTitle className="pb-2">{campaignTitle}</CardTitle>
@@ -55,7 +81,9 @@ export const CampaignCard = ({
           </div>
           <CardTitle>{completionPercentage}%</CardTitle>
         </div>
-        <CardDescription>{daysLeftToTarget} more days</CardDescription>
+        <CardDescription>
+          {daysLeftToTarget > 0 ? `${daysLeftToTarget} more days` : ''}
+        </CardDescription>
       </CardHeader>
       <CardContent>
         <Button className="mb-2">Make a Donation</Button>
