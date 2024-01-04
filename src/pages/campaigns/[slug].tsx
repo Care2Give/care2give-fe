aa3f56a2c8fddcde8ed5291bb3c58b0c16940c89
jianrong7 <ipwnjianrong@gmail.com>
2024-01-04 22:58:30 +0800

    fix checkout button

--- src/pages/campaigns/[slug].tsx
+++ src/pages/campaigns/[slug].tsx
@@ -64,6 +64,22 @@ const Campaign = ({ campaign, donationOptions }: CampaignProps) => {
   const [otherAmount, setOtherAmount] = useState<number | undefined>(undefined);
   const { addItem } = useCartStore();
 
+  const handleAddToGiftBasket = () => {
+    if (currentOption < 0 || currentOption >= donationOptions.length) {
+      return;
+    }
+    if (otherAmount && otherAmount < 10) {
+      return;
+    }
+    const option = donationOptions[currentOption];
+    addItem({
+      donationOption: option,
+      campaign,
+      otherAmount: otherAmount || 0,
+      isSelected: true,
+    });
+  };
+
   return (
     <>
       <NavBar title={campaignTitle || slugToTitle(slug)} />
@@ -217,35 +233,20 @@ const Campaign = ({ campaign, donationOptions }: CampaignProps) => {
             ))}
           </form>
           <div className="px-10 py-2 flex flex-col items-center gap-4">
-            <Link href="/gift-basket" className="max-w-80 w-full">
-              <Button className="rounded-full items-center text-2xl h-12 max-w-80 w-full">
-                Check Out
-              </Button>
-            </Link>
+            <Button
+              className="rounded-full items-center text-2xl h-12 max-w-80 w-full"
+              onClick={() => {
+                handleAddToGiftBasket();
+                router.push("/gift-basket");
+              }}
+            >
+              Check Out
+            </Button>
 
             <Button
-              className={cn(
-                `rounded-full flex gap-3 items-center text-2xl h-12 max-w-80`
-              )}
-              variant={"outline"}
-              onClick={() => {
-                if (
-                  currentOption < 0 ||
-                  currentOption >= donationOptions.length
-                ) {
-                  return;
-                }
-                if (otherAmount && otherAmount < 10) {
-                  return;
-                }
-                const option = donationOptions[currentOption];
-                addItem({
-                  donationOption: option,
-                  campaign,
-                  otherAmount: otherAmount || 0,
-                  isSelected: true,
-                });
-              }}
+              className="rounded-full flex gap-3 items-center text-2xl h-12 max-w-80"
+              variant="outline"
+              onClick={handleAddToGiftBasket}
             >
               <span>Add to Gift Basket</span>
               <Gift height={24} width={24} />
