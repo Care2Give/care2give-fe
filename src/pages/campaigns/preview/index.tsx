c2b7165de40a02c4d3d4040d4e6af0befa3fb06a
jianrong7 <ipwnjianrong@gmail.com>
2024-01-13 10:18:20 +0800

    fix types

--- src/pages/campaigns/preview/index.tsx
+++ src/pages/campaigns/preview/index.tsx
@@ -1,74 +1,93 @@
 import { useSearchParams } from "next/navigation";
-import {DonationOption } from "@/lib/campaignSample";
+import { DonationOption } from "@/lib/campaignSample";
 import Campaign from "@/pages/campaigns/[...slug]";
 
 export default function CampaignPreview() {
-    const searchParams = useSearchParams();
-    let campaignData;
-    try {
-        campaignData = getCampaignData(searchParams);
-    } catch(err) {
-        return(
-        <div>
-            <div>{err.toLocaleString()}</div>
-        </div>
-        );
-    }
-
+  const searchParams = useSearchParams();
+  let campaignData;
+  try {
+    campaignData = getCampaignData(searchParams);
+  } catch (err) {
     return (
-        <Campaign campaign={campaignData} />
+      <div>
+        <div>{err!.toLocaleString()}</div>
+      </div>
     );
+  }
+
+  return <Campaign campaign={campaignData as any} />;
 }
 
 function getCampaignData(searchParams: URLSearchParams) {
-    const requiredParameters: string[] =
-        ["title", "donors", "imageUrl", "currentAmount", "targetAmount", "targetDate", "slug", "description"];
-    for (let i = 0; i < requiredParameters.length; i++) {
-        const parameter = requiredParameters[i];
-        if (!searchParams.has(parameter)) {
-            throw new Error(`Missing required parameter: ${parameter}`);
-        }
+  const requiredParameters: string[] = [
+    "title",
+    "donors",
+    "imageUrl",
+    "currentAmount",
+    "targetAmount",
+    "targetDate",
+    "slug",
+    "description",
+  ];
+  for (let i = 0; i < requiredParameters.length; i++) {
+    const parameter = requiredParameters[i];
+    if (!searchParams.has(parameter)) {
+      throw new Error(`Missing required parameter: ${parameter}`);
     }
+  }
 
-    const numberParameters: string[] = ["donors", "currentAmount", "targetAmount", "targetDate"];
-    for (let i = 0; i < numberParameters.length; i++) {
-        const parameter = numberParameters[i];
-        if (isNaN(parseInt(searchParams.get(parameter)))) {
-            throw new Error(`${parameter} is not a valid integer`);
-        }
+  const numberParameters: string[] = [
+    "donors",
+    "currentAmount",
+    "targetAmount",
+    "targetDate",
+  ];
+  for (let i = 0; i < numberParameters.length; i++) {
+    const parameter = numberParameters[i];
+    if (isNaN(parseInt(searchParams.get(parameter) || ""))) {
+      throw new Error(`${parameter} is not a valid integer`);
     }
+  }
 
-    const title: string = searchParams?.get("title") || "";
-    const donors: number = parseInt(searchParams?.get("donors") || "0");
-    const imageUrl: string[] = searchParams.getAll("imageUrl");
-    const currentAmount: number = parseInt(searchParams.get("currentAmount") || "0");
-    const targetAmount: number = parseInt(searchParams.get("targetAmount") || "0");
-    const targetDate: number = parseInt(searchParams.get("targetDate") || "0");
-    const slug: string = searchParams.get("slug") || title;
-    const description: string = searchParams.get("description") || title;
-    return {
-        title: title,
-        donors: donors,
-        imageUrl: imageUrl,
-        currentAmount: currentAmount,
-        targetAmount: targetAmount,
-        targetDate: targetDate,
-        slug: slug,
-        description: description,
-        id: "",
-        donationAmounts: getDonationOptions(searchParams)
-    };
+  const title: string = searchParams?.get("title") || "";
+  const donors: number = parseInt(searchParams?.get("donors") || "0");
+  const imageUrl: string[] = searchParams.getAll("imageUrl");
+  const currentAmount: number = parseInt(
+    searchParams.get("currentAmount") || "0"
+  );
+  const targetAmount: number = parseInt(
+    searchParams.get("targetAmount") || "0"
+  );
+  const targetDate: number = parseInt(searchParams.get("targetDate") || "0");
+  const slug: string = searchParams.get("slug") || title;
+  const description: string = searchParams.get("description") || title;
+  return {
+    title: title,
+    donors: donors,
+    imageUrl: imageUrl,
+    currentAmount: currentAmount,
+    targetAmount: targetAmount,
+    targetDate: targetDate,
+    slug: slug,
+    description: description,
+    id: "",
+    donationAmounts: getDonationOptions(searchParams),
+  };
 }
 
 function getDonationOptions(searchParams: URLSearchParams): DonationOption[] {
-    const donationOptionValues: number[] = searchParams.getAll("donationOptionValue").map((donationOptionValue) => parseInt(donationOptionValue));
-    const donationOptionDescriptions: string[] = searchParams.getAll("donationOptionDescription");
-    return donationOptionValues.map((donationOptionValue, index) => {
-        return {
-            value: donationOptionValue,
-            description: donationOptionDescriptions[index]
-        }
-    });
+  const donationOptionValues: number[] = searchParams
+    .getAll("donationOptionValue")
+    .map((donationOptionValue) => parseInt(donationOptionValue));
+  const donationOptionDescriptions: string[] = searchParams.getAll(
+    "donationOptionDescription"
+  );
+  return donationOptionValues.map((donationOptionValue, index) => {
+    return {
+      value: donationOptionValue,
+      description: donationOptionDescriptions[index],
+    };
+  });
 }
 
 /*
