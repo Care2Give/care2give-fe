c156693f35931e630279cbd871bfc1389afa101c
jianrong7 <ipwnjianrong@gmail.com>
2024-06-01 10:34:39 +0800

    add favicon and title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Care2Give</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body>
         <Main />
